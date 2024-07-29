9ee9532606e16e4652083c4646b3e4b05e0ca0f8
Xusanjon <xusanmurodov907@gmail.com>
2024-07-29 23:02:20 +0500

    version 2

---
+++ src/app.module.ts
@@ -0,0 +1,44 @@
+import { Module } from "@nestjs/common";
+import { APP_GUARD, APP_GUARD } from "@nestjs/core";
+import { JwtModule } from "@nestjs/jwt";
+import { ThrottlerGuard, ThrottlerModule } from "@nestjs/throttler";
+import { AppController } from './app.controller';
+import { AuthModule } from './auth/auth.module';
+import { JwtAuthGuard } from "./auth/guard/jwt-auth.guard";
+import { CommnetModule } from './commnet/commnet.module';
+import { PostModule } from './post/post.module';
+import { PrismaService } from './prisma/prisma.service';
+import { UserModule } from './user/user.module';
+
+@Module({
+  imports: [
+    JwtModule.register({
+      global: true,
+      secret: "qwer12345",
+      signOptions: { expiresIn: '5m' },
+
+    }),
+    ThrottlerModule.forRoot([{
+      ttl: 60000,
+      limit: 50,
+    }]),
+    UserModule,
+    PostModule,
+    CommnetModule,
+    AuthModule
+  ],
+  controllers: [AppController],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard
+    },
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard
+    },
+    PrismaService
+
+  ],
+})
+export class AppModule { }
---
+++ src/user/user.controller.ts
@@ -0,0 +1,25 @@
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { SkipThrottle } from "@nestjs/throttler";
+import { SignUpAuthDto } from "src/auth/dto";
+import { UserService } from './user.service';
+
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) { }
+
+  @Post()
+  create(@Body() signUpDto: SignUpAuthDto) {
+    return this.userService.create(signUpDto);
+  }
+
+  @SkipThrottle()
+  @Get()
+  findAll(@Query('skip') skip: string, @Query('take') take: string) {
+    return this.userService.findAll(take, skip);
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.userService.findOne(+id);
+  }
+}
